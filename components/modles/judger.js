b5583d250c0a7124aa1317d75ca55186f30c9f95
peggy_kang <peggykang@163.com>
2021-02-26 17:44:20 +0800

    sku-padding

--- components/modles/judger.js
+++ components/modles/judger.js
@@ -1,14 +1,21 @@
 import {SkuCode} from "./sku-code";
+import {SkuPadding} from "./sku-padding";
 import {CellStatus} from '../../core/enum'
+import {Joiner} from '../../utils/joiner'
 
 class Judger{
 
     fenceGroup
     pathDic = []
+    skuPadding
 
     constructor(fenceGroup) {
         this.fenceGroup = fenceGroup
         this._initPathDic()
+    }
+
+    _initSkuPadding(){
+        this.skuPadding = new SkuPadding()
     }
 
     _initPathDic(){
@@ -20,26 +27,43 @@ class Judger{
 
     judger(cell){
         this._changeCurrentCellStatus(cell)
-        this.fenceGroup.eachCell(this._changeOtherCellStatus)
-    }
-
-    _changeOtherCellStatus(cell, x, y) {
-
+        this.fenceGroup.eachCell((cell, x, y)=>{
+            const path = this._findPotentialPath(cell, x, y)
+            console.log(path)
+        })
     }
 
     _findPotentialPath(cell, x, y){
+        const joiner = new Joiner()
         for (let i = 0; i < this.fenceGroup.fences.length; i++) {
-
+            const selected = this.skuPadding.findSelectedCellByX(i)
+            if (x === i) {
+                // 当前行
+                const cellCode = this._getCellCode(cell.spec)
+                joiner.join(cellCode)
+            } else {
+                if (selected) {
+                    const selectedCode = this._getCellCode(selected.spec)
+                    joiner.join(selectedCode)
+                }
+            }
         }
+        return joiner.getStr()
+    }
+
+    _getCellCode(spec){
+        return spec.key_id + "-" + spec.value_id
     }
 
     _changeCurrentCellStatus({cell, x, y}){
         if (cell.status === CellStatus.WAITING) {
-           this.fenceGroup.fences[x].cells[y].status = CellStatus.SELECTED
+            this.fenceGroup.fences[x].cells[y].status = CellStatus.SELECTED
+            this.skuPadding.insertCell(cell, x)
         }
 
         if (cell.status === CellStatus.SELECTED) {
             this.fenceGroup.fences[x].cells[y].status = CellStatus.WAITING
+            this.skuPadding.removeCell(x)
         }
     }
 
--- components/modles/sku-padding.js
+++ components/modles/sku-padding.js
@@ -5,9 +5,17 @@ class SkuPadding {
     constructor () {
 
     }
-    
-    insertCell () {
 
+    insertCell (cell, x) {
+        this.padding[x] = cell
+    }
+
+    removeCell (x) {
+        this.padding[x] = null
+    }
+
+    findSelectedCellByX (x) {
+        return this.padding[x]
     }
 
 }
